46ba8129b37a47c1490bd5b10cbbf44c3c1e3ae8
ecnuchensen <ecnuchensen@gmail.com>
2021-05-01 14:28:11 +0800

    add code

---
+++ code/run_rpk_explore_apk.py
@@ -0,0 +1,23 @@
+import os,sys
+import repkgApp, exploreAct_storydroid
+
+
+def execute(apk_path, apk_name, result_apkfolder, output):
+
+    print '======= Starting ' + apk_name + ' ========='
+    # repackge app
+    if not os.path.exists(os.path.join(result_apkfolder,apk_name+'.apk')):
+        r = repkgApp.startRepkg(apk_path, apk_name, result_apkfolder, output)
+        if r == 'no apk':
+            print 'apk not successfully recompiled!!!!!!!!!'
+
+    new_apkpath = os.path.join(result_apkfolder, apk_name+'.apk')
+
+    if os.path.exists(new_apkpath):
+        all_acts = exploreAct_storydroid.exploreAct(new_apkpath, apk_name, result_apkfolder)
+        return all_acts
+    else:
+        print 'No such repackaged app exist............'
+
+#execute('/home/senchen/Desktop/storydroid_plus/apks/org.liberty.android.fantastischmemo_223.apk','org.liberty.android.fantastischmemo_223',
+#        '/home/senchen/Desktop/storydroid_plus/outputs/org.liberty.android.fantastischmemo_223')
